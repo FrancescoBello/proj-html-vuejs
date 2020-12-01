46ade7d9764bc567a55f555b0fb0aa710577982a
Francesco <fraancesco.bello@gmail.com>
2020-12-01 13:53:43 +0100

    aggiunto percorso delle immagini
    aggiunti link nella navbar tramite struttura dati

---
+++ main.js
@@ -0,0 +1,14 @@
+var app = new Vue ({
+  el : "#root",
+  data : {
+    links : [
+      "Home",
+      "Culinary History",
+      "Our Team",
+      "Our Menu",
+      "Takeout",
+      "Bulletin",
+      "Reservation",
+    ],
+  },
+});
