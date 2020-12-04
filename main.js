23c0176e31a831e194eaa910a39d5e064776b19a
Francesco <fraancesco.bello@gmail.com>
2020-12-04 13:14:46 +0100

    finish

--- main.js
+++ main.js
@@ -1,20 +1,45 @@
 var app = new Vue ({
   el : "#root",
   data : {
-    links : ["Home",
-     "Culinary History",
-      "Our Team",
-      "Our Menu",
-      "Takeout",
-      "Bulletin",
-      "Reservation",
+   indice:0,
+    links : [{link: "Home",
+              indirizzo: "https://avada.theme-fusion.com/restaurant/" },
+              {link:"Culinary History",
+            indirizzo:"https://avada.theme-fusion.com/restaurant/culinary-history/"},
+            { link: "Our Team",
+          indirizzo:"https://avada.theme-fusion.com/restaurant/our-team/"},
+            {link:  "Our Menu",
+          indirizzo: "https://avada.theme-fusion.com/restaurant/our-seasonal-menu/"},
+              {link:"Takeout",
+            indirizzo:"https://avada.theme-fusion.com/restaurant/our-takeout-menu/"},
+              {link:"Bulletin",
+            indirizzo:"https://avada.theme-fusion.com/restaurant/bulletin/"},
+            {link:  "Reservation",
+          indirizzo:"https://avada.theme-fusion.com/restaurant/reservations/"},
     ],
+
+//    indirizzi : ["https://avada.theme-fusion.com/restaurant/",
+//                 "https://avada.theme-fusion.com/restaurant/culinary-history/",
+//                 "https://avada.theme-fusion.com/restaurant/our-team/",
+//                 "https://avada.theme-fusion.com/restaurant/our-seasonal-menu/",
+//                 "https://avada.theme-fusion.com/restaurant/our-takeout-menu/",
+//                 "https://avada.theme-fusion.com/restaurant/bulletin/",
+//                 "https://avada.theme-fusion.com/restaurant/reservations/",
+//    ],
+
     copyrights : ["Copyright 2012 - 2019 Avada |",
-     " All Rights Reserved |",
-     " Powered by WordPress |",
-     " ThemeFusion"
+                  " All Rights Reserved |",
+                  " Powered by WordPress |",
+                  " ThemeFusion"
    ],
+
    loghi : ["avada-restaurant-logo-new"],
 
   },
+  methods :{
+
+
+
+  },
+
 });
